77211e80c0582b8555d552b737f09c1012d4faa4
enki <enki.heart.kim@gmail.com>
2024-08-25 18:36:02 +0900

    Remove key

--- final/js/weather.js
+++ final/js/weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "aa0f3d3af4441fd9d13ea3c11bfa6291";
+const API_KEY = "";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
